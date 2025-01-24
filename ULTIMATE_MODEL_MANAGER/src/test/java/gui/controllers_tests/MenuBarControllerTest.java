a7d532c1bb1fa0c667a4458718b62de79d2a220b
MilkyFingers <micahbassett9@gmail.com>
2025-01-24 16:38:19 +0000

    menu bar thread safe

---
+++ ULTIMATE_MODEL_MANAGER/src/test/java/gui/controllers_tests/MenuBarControllerTest.java
@@ -0,0 +1,17 @@
+package gui.controllers_tests;
+
+import org.junit.jupiter.api.Nested;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class MenuBarControllerTest {
+	
+	@Nested
+	class HandleLoadTest extends ApplicationTest {
+		
+	}
+	
+	@Nested
+	class HandleSaveTest extends ApplicationTest {
+		
+	}
+}
