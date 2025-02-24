e47d55b9485e4e0f544ef5a7e5d3a38328390e3e
MilkyFingers <micahbassett9@gmail.com>
2025-02-24 13:51:37 +0000

    fxml and controller for adding dependency

--- ULTIMATE_MODEL_MANAGER/src/main/java/controllers/AddDependencyController.java
+++ ULTIMATE_MODEL_MANAGER/src/main/java/controllers/AddDependencyController.java
@@ -1,5 +1,54 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.Model;
+import parameters.DependencyParameter;
+import parameters.UncategorisedParameter;
+import project.Project;
+import sharedContext.SharedContext;
+import utils.Alerter;
+
 public class AddDependencyController {
 	
+	@FXML private ChoiceBox<UncategorisedParameter> undefinedParameters; // to list the ucs in the model
+	@FXML private ChoiceBox<Model> chooseModel; // choose from list of models in project
+	@FXML private TextField definition; // the definition of the dep param
+	@FXML private Button saveButton;
+	@FXML private Button cancelButton;
+	
+    private SharedContext sharedContext = SharedContext.getInstance();
+    private Project project = sharedContext.getProject();
+	
+	@FXML
+	public void initialize() {
+		undefinedParameters.setItems(project.getCurrentModel().getUncategorisedParameters());
+		chooseModel.setItems(project.getObservableModels());
+	}
+	
+	@FXML
+	private void saveDParam() {
+		String name = undefinedParameters.getValue().toString();
+		Model model = chooseModel.getValue();
+		String def = definition.getText();
+		if (name.equals("") || model.equals(null) || def.equals("")) {
+			Alerter.showErrorAlert("Invalid Parameter", "Please define all parameters!");
+			return;
+		}
+		else {
+			DependencyParameter depParam = new DependencyParameter(name, model, def);
+			project.getCurrentModel().addDependencyParameter(depParam);
+			project.getCurrentModel().removeUncategorisedParameter(undefinedParameters.getValue());
+			closeDialog();
+		}
+	}
+	
+	@FXML
+	private void closeDialog() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+	}
 }
