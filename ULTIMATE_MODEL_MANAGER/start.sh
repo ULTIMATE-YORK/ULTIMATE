b7390aa1b120547ccfba3b7b2294054daf20e7f3
MilkyFingers <micahbassett9@gmail.com>
2025-02-26 14:33:44 +0000

    update bash

--- ULTIMATE_MODEL_MANAGER/start.sh
+++ ULTIMATE_MODEL_MANAGER/start.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 # build the project
 mvn clean install
-mvn package
+echo "{}" >> config.json
 java --module-path $SCRIPT_DIR/libs/javafx-sdk-23.0.1/lib --add-modules javafx.controls,javafx.fxml -jar $SCRIPT_DIR/target/ultimate_model_manager-0.0.1-SNAPSHOT.jar
