fc7f7e29db99ba57e79031e93cf06af562434017
MilkyFingers <micahbassett9@gmail.com>
2025-02-26 12:48:01 +0000

    script to launch tool

---
+++ ULTIMATE_MODEL_MANAGER/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+# build the project
+mvn clean install
+mvn package
+java --module-path $SCRIPT_DIR/libs/javafx-sdk-23.0.1/lib --add-modules javafx.controls,javafx.fxml -jar $SCRIPT_DIR/target/ultimate_model_manager-0.0.1-SNAPSHOT.jar
