cbba545020839ddec3919d14280d3545c5e67c83
MilkyFingers <micahbassett9@gmail.com>
2025-03-05 12:59:34 +0000

    changed font to 13 px

--- ULTIMATE_MODEL_MANAGER/src/main/java/utils/Font.java
+++ ULTIMATE_MODEL_MANAGER/src/main/java/utils/Font.java
@@ -2,7 +2,7 @@ package utils;
 
 public class Font {
 	
-	public static final String MODEL_LIST_FONT = "-fx-font-size: 18px;";
-	public static final String UC_LIST_FONT = "-fx-font-size: 18px;";
+	public static final String MODEL_LIST_FONT = "-fx-font-size: 13px;";
+	public static final String UC_LIST_FONT = "-fx-font-size: 13px;";
 
 }
