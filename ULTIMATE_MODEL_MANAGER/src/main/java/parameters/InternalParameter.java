bae7358469fb69965a221bd87d4c80fb353811da
MilkyFingers <micahbassett9@gmail.com>
2025-05-06 11:53:24 +0100

    internal parameter defined

--- ULTIMATE_MODEL_MANAGER/src/main/java/parameters/InternalParameter.java
+++ ULTIMATE_MODEL_MANAGER/src/main/java/parameters/InternalParameter.java
@@ -5,12 +5,14 @@ public class InternalParameter {
 	private String type;
 	private Double min;
 	private Double max;
+	private Double interval;
 	
-	public InternalParameter(String name, String type, Double min, Double max) {
+	public InternalParameter(String name, String type, Double min, Double max, Double interval) {
 		this.name = name;
 		this.type = type;
 		this.min = min;
 		this.max = max;
+		this.interval = interval;
 	}
 	
     // GETTER METHODS
@@ -30,6 +32,10 @@ public class InternalParameter {
 	public Double getMax() {
 		return this.max;
 	}
+	
+	public Double getInterval() {
+		return this.interval;
+	}
     
     // SETTER METHODS
     
@@ -40,10 +46,18 @@ public class InternalParameter {
     public void setType(String type) {
 		this.type = type;
     }
-    
-	public void setRange(Double min, Double max) {
+	
+	public void setInterval(Double interval) {
+		this.interval = interval;
+	}
+
+	public void setMin(Double min) {
 		this.min = min;
-    }
+	}
+
+	public void setMax(Double max) {
+		this.max = max;
+	}
     
     public String toString() {
     	return getName();
